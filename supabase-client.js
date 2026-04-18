f1f78a82f6c62ae3a60975e2aac635825d808c54
aablay029pbox-dotcom <aablay029.pbox@parsu.edu.ph>
2026-04-18 16:21:27 +0800

    Update Supabase URL and anon key

--- supabase-client.js
+++ supabase-client.js
@@ -6,8 +6,8 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
 
 // ====== CONFIGURE THESE ======
-export const SUPABASE_URL = 'https://glficbqlwjxedlbovdtn.supabase.co';
-export const SUPABASE_ANON_KEY = 'sb_publishable_J2Txi6Dt26H0JK_SIvIO2w_esti_hDr';
+export const SUPABASE_URL = 'https://cejndnbrskzqqvapehtf.supabase.co';
+export const SUPABASE_ANON_KEY = 'sb_publishable_6KOGl3wRnPRYyXO0LKNQMA_6vDugKri';
 // =============================
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
